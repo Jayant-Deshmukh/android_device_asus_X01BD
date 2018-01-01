46ae4afb31eb646550ebd4c023213c6ac8eb6f41
Wei Wang <wvw@google.com>
2017-12-31 22:14:16 -0800

    X01BD: power: Switch to libperfmgr for powerhint
    Test: boot and do powerhint
    Bug: 62041945
    Change-Id: I7de1d2bf377fb46162171a084fca3413b1067d3b

--- device.mk
+++ device.mk
@@ -368,7 +368,7 @@ PRODUCT_PACKAGES += \
 
 # Powerhint
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # RCS
 PRODUCT_PACKAGES += \
@@ -424,8 +424,8 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
-	
+    android.hardware.power@1.2-service.x01bd-libperfmgr
+
 # RenderScript HAL
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
