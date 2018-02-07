19ea7f8b993726f58e0c65bbe5f3f451af785c88
Bruno Martins <bgcngm@gmail.com>
2018-02-07 00:41:09 +0000

    X01BD: Use MIUI poweroff charging animation
     * It's cool, and more importantly... it actually works!
       Oh look, it even displays battery level. Wow. :-P
    
    Change-Id: I6351e2eac6df89aa1b6b98e4e6b2815fa5c37794

--- device.mk
+++ device.mk
@@ -304,7 +304,8 @@ PRODUCT_PACKAGES += \
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.0-impl \
-    android.hardware.health@2.0-service
+    android.hardware.health@2.0-service \
+    chargeonlymode
 
 # HIDL
 PRODUCT_PACKAGES += \
