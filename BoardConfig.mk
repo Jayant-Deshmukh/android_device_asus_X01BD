6e6fd89863d9b1b195e2efb639644fc159414a1a
kubersharma001 <kubersharma001@gmail.com>
2019-02-28 22:16:54 +0100

    X01BD: [TEMP] set SELINUX_IGNORE_NEVERALLOWS := true
    Change-Id: I05001df952d5d5dfc09dc0dbfde713009341a940

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -253,10 +253,13 @@ BOARD_SECCOMP_POLICY := $(DEVICE_PATH)/seccomp
 
 # Security patch level
 VENDOR_SECURITY_PATCH := 2018-12-05
+
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 BOARD_SEPOLICY_VERS := 28.0
+SELINUX_IGNORE_NEVERALLOWS := true
+
 # Tap to wake
 TARGET_TAP_TO_WAKE_NODE := /proc/tpd_gesture
 
