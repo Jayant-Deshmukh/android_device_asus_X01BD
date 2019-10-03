e9b9edcb3ee28ad519d74ff1ac80e1ab5d275d4f
Sonal Singh <sonal.singh.19993@gmail.com>
2019-10-03 05:05:14 +0000

    X01BD: Add minimal sepolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -230,8 +230,9 @@ BOARD_SECCOMP_POLICY := $(DEVICE_PATH)/seccomp
 VENDOR_SECURITY_PATCH := 2018-06-05
 
 # SELinux
-include device/qcom/sepolicy-legacy-um/sepolicy.mk
+#include device/qcom/sepolicy-legacy-um/sepolicy.mk
 #BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy-minimal
 
 # WiFi
 BOARD_HAS_QCOM_WLAN := true
