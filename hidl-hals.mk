3108e60d30353eb00a4a7dff91fff2fc6bd82533
Sonal Singh <sonal.singh.19993@gmail.com>
2019-10-05 17:47:01 +0000

    X01BD: Nuke VR

--- hidl-hals.mk
+++ hidl-hals.mk
@@ -125,11 +125,6 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
     android.hardware.vibrator@1.0-service
 
-# VR
-PRODUCT_PACKAGES += \
-    android.hardware.vr@1.0-impl \
-    android.hardware.vr@1.0-service
-
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service
